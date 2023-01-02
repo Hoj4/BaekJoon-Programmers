ad180209f2b87fcf42781c6572448e273d566133
Hoj4 <118800372+Hoj4@users.noreply.github.com>
2023-01-02 23:58:58 +0900

    [D1] Title: 신문 헤드라인, Time: 121 ms, Memory: 20,568 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	        String a = sc.nextLine();
+       		 System.out.println(a.toUpperCase());
+
+    }
+}
