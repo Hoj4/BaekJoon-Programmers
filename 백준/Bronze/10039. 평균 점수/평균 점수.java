09c7bb4124322b158b558fe7cb5bfb8cf2aba887
Hoj4 <118800372+Hoj4@users.noreply.github.com>
2023-01-06 19:19:19 +0900

    [Bronze IV] Title: 평균 점수, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0;
+		
+		for(int i=0 ; i < 5 ; i++) {
+			int score = Integer.parseInt(br.readLine());
+			
+			 if(score > 40){
+			 	sum = sum + score;
+			 } else {
+			 	sum = sum + 40;
+			 }
+			
+		}
+		System.out.println(sum/5);
+	}
+ 
+}
