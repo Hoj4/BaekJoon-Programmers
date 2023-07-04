e507cb1682fc9d5f248d3f61de21b98131536025
임호정's Git <118800372+Hoj4@users.noreply.github.com>
2023-07-05 00:03:03 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.nextLine();
+		
+		for(int i=0 ; i<A.length() ; i++) {
+				System.out.print(A.charAt(i));
+			if(i%10 == 9) {
+				System.out.println();
+			}
+		}
+	}
+}
