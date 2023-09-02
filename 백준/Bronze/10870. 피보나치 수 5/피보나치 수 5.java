de7cbff69fb1554d68f3c63171d6d7f0e9eb6f04
임호정's Git <118800372+Hoj4@users.noreply.github.com>
2023-09-02 20:06:35 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(fibo(N));
+	}
+ 
+	static int fibo(int N) {
+		if(N==0){
+            return 0;
+        }
+		if(N==1)	{
+            return 1;
+        }
+		return fibo(N - 1) + fibo(N - 2);
+	}
+}
